553f095805a03bdea2762689de03036042915469
Denis Korolev <den.korolev.1999@bk.ru>
2021-11-26 18:13:24 +0300

    1.2

--- src/pokemons/Mudsdale.java
+++ src/pokemons/Mudsdale.java
@@ -1,13 +1,11 @@
 package pokemons;
 
 import moves.*;
-import ru.ifmo.se.pokemon.*;
 
 public class Mudsdale extends Mudbray {
     public Mudsdale(String name, int level) {
         super(name, level);
         setStats(100, 125, 100, 55, 85, 35);
-        setType(Type.GROUND);
         addMove(new RockSlide());
     }
 }
